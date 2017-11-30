c40f00046b997b9438f8e212795e89f4cac84fd0
mobone <nicholas.reichel@gmail.com>
2017-11-29 22:22:03 -0600

    Added multiprocessing

--- nosql_ml.py
+++ nosql_ml.py
@@ -8,70 +8,55 @@ from time import sleep
 from sklearn import preprocessing
 from sklearn.externals import joblib
 import numpy as np
+from multiprocessing import Process, Queue
 
-class machine(object):
-    def __init__(self,features, data, hold_time):
-        conn = sqlite3.connect('gap_data.db')
-        if 'Volume' in features: features.remove('Volume')
-        if 'Avg Volume' in features: features.remove('Avg Volume')
-        if 'Income' in features: features.remove('Income')
-        self.sim_count = 4
+class machine(Process):
+    def __init__(self,process_q):
+        Process.__init__(self)
+        self.process_q = process_q
+        self.sim_count = 6
         self.trade_count = 10
         self.bins = 2
-        self.machine='Classifier'
-        self.orignal_data = data
-        self.hold_time = hold_time
-        self.target = features[len(features)-1]
 
-        feature_set = []
-        for i in range(3,6):
-            for machine in ['Classifier']:
-                for feature in list(itertools.permutations(features[:-1], r=i)):
-                    feature_set.append([machine,feature])
-        shuffle(feature_set)
 
-        print('starting', len(feature_set))
-        for self.machine, feature in feature_set:
-            self.data = self.orignal_data.copy()
-            self.features = list(feature)
+    def run(self):
+        self.conn = sqlite3.connect('gap_data.db')
+        self.original_data = pd.read_csv('nosql_data.csv')
+        while self.process_q.qsize():
+            self.target,self.machine,self.features = self.process_q.get()
+            self.features = list(self.features)
+            self.data = self.original_data[self.features+[self.target]].dropna()
+
             self.negative = []
             self.positive = []
             self.predictions_neg = []
             self.predictions_pos = []
-            sleep(.01)
+
             for self.sim_num in range(self.sim_count):
                 self.get_data()
                 self.get_model()
                 self.get_predictions()
 
-                if len(set(self.predictions))<10 and self.machine=='Regression':
+                if len(set(self.predictions))<5 and self.machine=='Regression':
                     break
 
             self.get_metrics()
 
             # discard models with unsatisfactory metrics
-            if self.metric_df['Pos_Mean'][0]<.02 or self.metric_df['Neg_Mean'][0]>-.02:
+            if self.metric_df['Pos_Mean'][0]<.025 or self.metric_df['Neg_Mean'][0]>-.025:
                 continue
-            if self.machine=='Regression':
-                if len(self.negative) != self.sim_count*self.trade_count:
-                    continue
-                if self.metric_df['Neg_Cutoff'][0]>self.metric_df['Pos_Cutoff'][0]:
-                    continue
-            elif self.machine=='Classifier':
+
+            if self.machine=='Classifier':
                 if len(self.negative)<self.sim_count*(self.trade_count-3):
                     continue
                 if len(self.positive)<self.sim_count*(self.trade_count-3):
-                    continue
-
-                if self.metric_df['Diff_Mean'][0]<0 or self.metric_df['Diff_Median'][0]<0:
                     continue
 
             # save model if diff is great enough
             if self.metric_df['Diff_Mean'][0]>.05 and self.metric_df['Diff_Median'][0]>.03:
                 self.save_model()
 
-            print(self.metric_df)
-            self.metric_df.to_sql('nosql_data_pruned', conn, if_exists='append',index=False)
+            self.metric_df.to_sql('nosql_data_pruned', self.conn, if_exists='append',index=False)
 
     def save_model(self):
         self.get_out_model()
@@ -82,28 +67,28 @@ class machine(object):
         joblib.dump(self.out_clf, 'models/%s.pkl' % filename)
 
     def get_metrics(self):
-            df1 = pd.DataFrame({'Neg_Cutoff': self.predictions_neg})
-            df2 = pd.DataFrame({'Pos_Cutoff': self.predictions_pos})
-            df3 = pd.DataFrame({'Negative': self.negative})
-            df4 = pd.DataFrame({'Positive': self.positive})
+        df1 = pd.DataFrame({'Neg_Cutoff': self.predictions_neg})
+        df2 = pd.DataFrame({'Pos_Cutoff': self.predictions_pos})
+        df3 = pd.DataFrame({'Negative': self.negative})
+        df4 = pd.DataFrame({'Positive': self.positive})
 
-            df = pd.concat([df1,df2,df3,df4], ignore_index=True,axis=1)
-            df.columns = ['Neg_Cutoff', 'Pos_Cutoff', 'Negative', 'Positive']
-            metrics = df.describe().transpose()
+        df = pd.concat([df1,df2,df3,df4], ignore_index=True,axis=1)
+        df.columns = ['Neg_Cutoff', 'Pos_Cutoff', 'Negative', 'Positive']
+        metrics = df.describe().transpose()
 
-            row = [self.machine, '_'.join(self.features)]
-            row.extend([metrics['max']['Neg_Cutoff'], metrics['min']['Pos_Cutoff'],
-                       metrics['count']['Negative'], metrics['count']['Negative'],
-                       metrics['mean']['Negative'], metrics['50%']['Negative'],
-                       metrics['mean']['Positive'], metrics['50%']['Positive']])
+        row = [self.machine, '_'.join(self.features)]
+        row.extend([metrics['max']['Neg_Cutoff'], metrics['min']['Pos_Cutoff'],
+                   metrics['count']['Negative'], metrics['count']['Negative'],
+                   metrics['mean']['Negative'], metrics['50%']['Negative'],
+                   metrics['mean']['Positive'], metrics['50%']['Positive']])
 
-            df = pd.DataFrame([row], columns = ['Machine_Type','Features', 'Neg_Cutoff', 'Pos_Cutoff',
-                                                'Neg_Count','Pos_Count', 'Neg_Mean',
-                                                'Neg_Median', 'Pos_Mean', 'Pos_Median'])
-            df['Diff_Mean'] = df['Pos_Mean'] + (df['Neg_Mean']*-1)
-            df['Diff_Median'] = df['Pos_Median'] + (df['Neg_Median']*-1)
-            df['Hold_Time'] = self.hold_time
-            self.metric_df = df
+        df = pd.DataFrame([row], columns = ['Machine_Type','Features', 'Neg_Cutoff', 'Pos_Cutoff',
+                                            'Neg_Count','Pos_Count', 'Neg_Mean',
+                                            'Neg_Median', 'Pos_Mean', 'Pos_Median'])
+        df['Diff_Mean'] = df['Pos_Mean'] + (df['Neg_Mean']*-1)
+        df['Diff_Median'] = df['Pos_Median'] + (df['Neg_Median']*-1)
+        df['Hold_Time'] = self.target
+        self.metric_df = df
 
     def bin_data(self):
         self.data['Bin'] = np.round(self.data[self.target].rank(pct=True)*(self.bins))
@@ -157,22 +142,39 @@ class machine(object):
             self.predictions_pos.extend(self.a_test['Predicted'].tail(self.trade_count).values)
 
 
-correls = pd.read_csv('corr.csv')
-df = pd.read_csv('nosql_data.csv')
-correls.index = correls[correls.columns[0]]
-correls = correls.ix[:-5]
+if __name__ == '__main__':
+    process_q = Queue()
 
-features = ['Perf Half Y', 'Beta', 'Inst Trans', 'Sales Q\/Q', 'Gross Margin',
-            'P\/S', 'EPS next 5Y', 'Perf YTD', 'P\/B', 'Sales past 5Y',
-            'RSI (14)', 'Perf Quarter', 'SMA20', 'Recom', 'EPS past 5Y',
-            'ROI', 'Change', 'Perf Year']
+    features = ['Perf Half Y', 'Beta', 'Inst Trans', 'Sales Q\/Q', 'Gross Margin',
+                'P\/S', 'EPS next 5Y', 'Perf YTD', 'P\/B', 'Sales past 5Y',
+                'RSI (14)', 'Perf Quarter', 'SMA20', 'Recom', 'EPS past 5Y',
+                'ROI', 'Change', 'Perf Year']
 
-for i in range(5,6):
+
     if features is None:
+        correls = pd.read_csv('corr.csv')
+        correls.index = correls[correls.columns[0]]
+        correls = correls.ix[:-5]
         corr = pd.DataFrame(correls.ix[:,'Day '+str(i)])
         features = list(corr[corr['Day '+str(i)].abs() >.03].index)
 
-    features.extend(['Day '+str(i)])
-    data = df[features].dropna()
+    feature_set = []
+    for permute_length in range(3,7):
+        for num_days in range(4,6):
+            for machine_type in ['Regression']:
+                for feature in list(itertools.permutations(features[:-1], r=permute_length)):
+                    feature_set.append(['Day '+str(num_days),machine_type,feature])
+    shuffle(feature_set)
+    for feature in feature_set:
+        process_q.put(feature)
+    print(process_q.qsize())
 
-    machine(features, data, i)
+    for process_id in range(8):
+        p = machine(process_q)
+        p.start()
+
+    while process_q.qsize():
+        sleep(60)
+        print(process_q.qsize())
+    while process_q.qsize():
+        process_q.get()
