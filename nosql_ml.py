8c097d382cdef255519465106ee4819eef482739
mobone <nicholas.reichel@gmail.com>
2017-11-29 22:42:47 -0600

    Comments added

--- nosql_ml.py
+++ nosql_ml.py
@@ -13,58 +13,103 @@ from multiprocessing import Process, Queue
 class machine(Process):
     def __init__(self,process_q):
         Process.__init__(self)
+        # get the processing queue
         self.process_q = process_q
-        self.sim_count = 6
-        self.trade_count = 10
-        self.bins = 2
+
+        # set initial params, universal to all processors
+        self.sim_count = 6      # n experiements per model
+        self.trade_count = 10   # set the number of trades per model
+        self.bins = 2           # used for classifiers, chooses the bin size
 
 
     def run(self):
+        # connect to db for storage and read the input data csv
         self.conn = sqlite3.connect('gap_data.db')
         self.original_data = pd.read_csv('nosql_data.csv')
+
+        # iterate over the process queue
         while self.process_q.qsize():
             self.target,self.machine,self.features = self.process_q.get()
             self.features = list(self.features)
+            # select the data specific to this feature set
             self.data = self.original_data[self.features+[self.target]].dropna()
 
+            # used for storing the results of n experiements
             self.negative = []
             self.positive = []
             self.predictions_neg = []
             self.predictions_pos = []
 
+            # run n experiements
             for self.sim_num in range(self.sim_count):
-                self.get_data()
-                self.get_model()
-                self.get_predictions()
+                self.get_data()     # splits data into random train/test splits
+                self.get_model()    # trains the Classifier or Regression model
+                self.get_predictions()  # uses the test input to get output predictions
 
+                # discard the model if its regression and producing only one prediction
                 if len(set(self.predictions))<5 and self.machine=='Regression':
                     break
 
+            # take in the results from the experience and get stats about performance
             self.get_metrics()
-
+            
             # discard models with unsatisfactory metrics
             if self.metric_df['Pos_Mean'][0]<.025 or self.metric_df['Neg_Mean'][0]>-.025:
                 continue
-
             if self.machine=='Classifier':
                 if len(self.negative)<self.sim_count*(self.trade_count-3):
                     continue
                 if len(self.positive)<self.sim_count*(self.trade_count-3):
                     continue
 
-            # save model if diff is great enough
+            # save model if diff between neg predictions and pos predictions is great enough
+            # this model is fit using 100% of the data, and will be used to make future predictions
             if self.metric_df['Diff_Mean'][0]>.05 and self.metric_df['Diff_Median'][0]>.03:
                 self.save_model()
 
+            # store result in the database
             self.metric_df.to_sql('nosql_data_pruned', self.conn, if_exists='append',index=False)
 
-    def save_model(self):
-        self.get_out_model()
-        filename = [self.metric_df['Features'][0].replace(' ','-').replace('\/', '-o-'),
-                    str(float(self.metric_df['Neg_Cutoff'][0])),
-                    str(float(self.metric_df['Pos_Cutoff'][0]))]
-        filename = '__'.join(filename)
-        joblib.dump(self.out_clf, 'models/%s.pkl' % filename)
+    def get_data(self):
+        if self.machine=='Classifier':
+            self.bin_data()
+            split_data = train_test_split(preprocessing.scale(self.data[self.features]),
+                                          self.data.ix[:,['Bin',self.target]], test_size=.4)
+        elif self.machine=='Regression':
+            split_data = train_test_split(preprocessing.scale(self.data[self.features]),
+                                          self.data[self.target], test_size=.4)
+        self.a_train, self.a_test, self.b_train, self.b_test = split_data
+
+    def bin_data(self):
+        # this turns continuous data into n classes, for us in classifier
+        self.data['Bin'] = np.round(self.data[self.target].rank(pct=True)*(self.bins))
+
+    def get_model(self):
+        if self.machine=='Classifier':
+            self.clf = SVC(C=1.0)
+            self.clf.fit(self.a_train, self.b_train['Bin'])
+        elif self.machine=='Regression':
+            self.clf = SVR(C=1.0, epsilon=0.2)
+            self.clf.fit(self.a_train, self.b_train)
+
+    def get_predictions(self):
+        self.predictions = self.clf.predict(self.a_test)
+        self.a_test = pd.DataFrame(self.a_test, columns = self.features)
+        self.a_test['Predicted'] = list(self.predictions)
+
+        self.a_test = self.a_test.sort_values(by=['Predicted'])
+        if self.machine=='Classifier':
+            self.a_test['Return'] = self.b_test[self.target].values
+            self.negative.extend(self.a_test[self.a_test['Predicted']==0]['Return'].values)
+            self.positive.extend(self.a_test[self.a_test['Predicted']==self.bins]['Return'].values)
+            self.predictions_neg.extend(self.a_test[self.a_test['Predicted']==0]['Predicted'].values)
+            self.predictions_pos.extend(self.a_test[self.a_test['Predicted']==self.bins]['Predicted'].values)
+        elif self.machine=='Regression':
+            self.a_test['Return'] = self.b_test.values
+            self.negative.extend(self.a_test['Return'].head(self.trade_count).values)
+            self.positive.extend(self.a_test['Return'].tail(self.trade_count).values)
+            self.predictions_neg.extend(self.a_test['Predicted'].head(self.trade_count).values)
+            self.predictions_pos.extend(self.a_test['Predicted'].tail(self.trade_count).values)
 
     def get_metrics(self):
         df1 = pd.DataFrame({'Neg_Cutoff': self.predictions_neg})
@@ -90,26 +135,13 @@ class machine(Process):
         df['Hold_Time'] = self.target
         self.metric_df = df
 
-    def bin_data(self):
-        self.data['Bin'] = np.round(self.data[self.target].rank(pct=True)*(self.bins))
-
-    def get_data(self):
-        if self.machine=='Classifier':
-            self.bin_data()
-            split_data = train_test_split(preprocessing.scale(self.data[self.features]),
-                                          self.data.ix[:,['Bin',self.target]], test_size=.4)
-        elif self.machine=='Regression':
-            split_data = train_test_split(preprocessing.scale(self.data[self.features]),
-                                          self.data[self.target], test_size=.4)
-        self.a_train, self.a_test, self.b_train, self.b_test = split_data
-
-    def get_model(self):
-        if self.machine=='Classifier':
-            self.clf = SVC(C=1.0)
-            self.clf.fit(self.a_train, self.b_train['Bin'])
-        elif self.machine=='Regression':
-            self.clf = SVR(C=1.0, epsilon=0.2)
-            self.clf.fit(self.a_train, self.b_train)
+    def save_model(self):
+        self.get_out_model()
+        filename = [self.metric_df['Features'][0].replace(' ','-').replace('\/', '-o-'),
+                    str(float(self.metric_df['Neg_Cutoff'][0])),
+                    str(float(self.metric_df['Pos_Cutoff'][0]))]
+        filename = '__'.join(filename)
+        joblib.dump(self.out_clf, 'models/%s.pkl' % filename)
 
     def get_out_model(self):
         if self.machine=='Classifier':
@@ -121,25 +153,7 @@ class machine(Process):
             self.out_clf.fit(preprocessing.scale(self.data[self.features]),
                              self.data[self.target])
 
-    def get_predictions(self):
-        self.predictions = self.clf.predict(self.a_test)
-        self.a_test = pd.DataFrame(self.a_test, columns = self.features)
-        self.a_test['Predicted'] = list(self.predictions)
 
-
-        self.a_test = self.a_test.sort_values(by=['Predicted'])
-        if self.machine=='Classifier':
-            self.a_test['Return'] = self.b_test[self.target].values
-            self.negative.extend(self.a_test[self.a_test['Predicted']==0]['Return'].values)
-            self.positive.extend(self.a_test[self.a_test['Predicted']==self.bins]['Return'].values)
-            self.predictions_neg.extend(self.a_test[self.a_test['Predicted']==0]['Predicted'].values)
-            self.predictions_pos.extend(self.a_test[self.a_test['Predicted']==self.bins]['Predicted'].values)
-        elif self.machine=='Regression':
-            self.a_test['Return'] = self.b_test.values
-            self.negative.extend(self.a_test['Return'].head(self.trade_count).values)
-            self.positive.extend(self.a_test['Return'].tail(self.trade_count).values)
-            self.predictions_neg.extend(self.a_test['Predicted'].head(self.trade_count).values)
-            self.predictions_pos.extend(self.a_test['Predicted'].tail(self.trade_count).values)
 
 
 if __name__ == '__main__':
@@ -150,7 +164,7 @@ if __name__ == '__main__':
                 'RSI (14)', 'Perf Quarter', 'SMA20', 'Recom', 'EPS past 5Y',
                 'ROI', 'Change', 'Perf Year']
 
-
+    # ability to toggle preset features vs finding those that have the highest correlation
     if features is None:
         correls = pd.read_csv('corr.csv')
         correls.index = correls[correls.columns[0]]
@@ -158,23 +172,36 @@ if __name__ == '__main__':
         corr = pd.DataFrame(correls.ix[:,'Day '+str(i)])
         features = list(corr[corr['Day '+str(i)].abs() >.03].index)
 
+
     feature_set = []
-    for permute_length in range(3,7):
+    # permute all the different combinations of the possible features, of length 3 to n
+    for permute_length in range(3,4):
+        #hold stock for 4, 5 or 6 days, this is our target variable
         for num_days in range(4,6):
+            # ability to change from Classifier and Regression models
             for machine_type in ['Regression']:
                 for feature in list(itertools.permutations(features[:-1], r=permute_length)):
                     feature_set.append(['Day '+str(num_days),machine_type,feature])
+
+    # shuffle the list so that the multiprocessing can discover good vs bad models randomly
     shuffle(feature_set)
+
+    # put them in the queue
     for feature in feature_set:
         process_q.put(feature)
+
     print(process_q.qsize())
 
+    # start the processors
     for process_id in range(8):
         p = machine(process_q)
         p.start()
 
+    # wait for the queue to empty
     while process_q.qsize():
         sleep(60)
         print(process_q.qsize())
+
+    # if ctrl+c is encountered, the queue must be cleared before terminating
     while process_q.qsize():
         process_q.get()
