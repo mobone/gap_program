899ed40764d73a9d16c06f39e132ff102d42f248
Nick Reichel <nreichel@sciencelogic.com>
2017-11-30 10:14:20 -0600

    Completed integration of nosql dataset into alerts

--- finviz_alerts.py
+++ finviz_alerts.py
@@ -3,6 +3,7 @@ import requests as r
 from requests_toolbelt.threaded import pool
 from datetime import datetime, date, timedelta
 from sklearn.externals import joblib
+from sklearn import preprocessing
 import warnings
 import re
 from nyse_holidays import *
@@ -16,10 +17,9 @@ class finviz_alerts(object):
     runs their data through prediction, stores in database
     """
     def __init__(self):
-
         conn = sqlite3.connect('gap_data.db')
         # get machines
-        sql = 'select * from nosql_data_pruned where Neg_Median<-.01 and Pos_Median>.01 order by diff_mean desc limit 20;'
+        sql = 'select * from nosql_data_pruned where Neg_Median<-.01 and Pos_Median>.01 order by diff_mean desc limit 25;'
         machines = pd.read_sql(sql,conn).ix[:,['Features','Neg_Cutoff','Pos_Cutoff','Hold_Time']]
 
         # get all features we might use
@@ -27,57 +27,51 @@ class finviz_alerts(object):
         for machine in machines['Features'].values:
             machine = machine.replace('\\','')
             features_list.extend(machine.split('_'))
-
         self.total_features_set = list(set(features_list))
+
         self.get_backtest_dataset()
 
         print("Getting alerts", datetime.now())
         self.get_alerts()
         self.company_data = pd.concat([self.total_dataset, self.company_data])
 
-
         for self.features,self.neg_cutoff,self.pos_cutoff,self.hold_days in machines.values:
-            self.features = '['+self.features.replace('\/','/').replace(' ',', ')+']'
-            print(self.features)
-
-            self.machine_id = [self.features.replace('/','-o-').replace(' ','-'),
+            self.hold_days = int(self.hold_days.split(' ')[1])
+            self.features = self.features.replace('\/','/').split('_')
+            self.machine_id = ['_'.join(self.features).replace('/','-o-').replace(' ','-'),
                                str(self.neg_cutoff), str(self.pos_cutoff)]
             self.machine_id = '__'.join(self.machine_id)
-            print(self.machine_id)
-            input()
 
             self.get_machine()
             self.get_predictions()
             self.get_close_dates()
-
-            self.companies.to_sql(self.machine_id.split('_')[0], conn, if_exists='append', index=False)
-            break
+            print(self.companies)
+            #self.companies.to_sql(self.machine_id.split('_')[0], conn, if_exists='append', index=False)
 
     def get_close_dates(self):
         for company in self.companies.iterrows():
 
             start_date = datetime.strptime(company[1]['Start_Date'], '%b %d, %Y')
 
-            for i in range(self.hold_days-1):
+            for i in range(self.hold_days):
                 start_date = start_date+timedelta(days=1)
                 while start_date.strftime('%y%m%d') == NYSE_holidays()[0].strftime('%y%m%d') or start_date.weekday()>=5:
                     start_date = start_date+timedelta(days=1)
 
             self.companies.loc[company[0],'Close_Date'] = start_date.strftime('%b %d, %Y')
+            self.companies.loc[company[0],'Hold_Days'] = self.hold_days
             self.companies.loc[company[0],'Close_Price'] = None
             self.companies.loc[company[0],'Percent_Change'] = None
-            self.companies.loc[company[0],'Hold_Days'] = self.hold_days
 
     def get_predictions(self):
-        self.companies = self.company_data[self.features]
-        self.companies = self.companies.dropna()
-        print(self.companies)
-        exit()
+        self.companies = self.company_data[self.features+['Ticker','Start_Date','Price']]
+        self.companies = self.companies.dropna(subset=self.features)
         self.companies['Play'] = None
-        self.companies['Prediction'] = self.clf.predict(self.companies[['Signal', 'Wk', 'Mth', 'Vol_Chg']])
+        self.companies['Prediction'] = self.clf.predict(preprocessing.scale(self.companies[self.features]))
         self.companies.loc[self.companies['Prediction']<self.neg_cutoff, 'Play'] = 'Short'
         self.companies.loc[self.companies['Prediction']>self.pos_cutoff, 'Play'] = 'Long'
-        self.companies = self.companies.dropna(subset=['Play'])
+        self.companies = self.companies.dropna(subset=['Play','Ticker'])
+        self.companies.index = self.companies['Ticker']
 
     def get_machine(self):
         self.clf = joblib.load('models/%s.pkl' % self.machine_id)
@@ -104,16 +98,12 @@ class finviz_alerts(object):
                     df = pd.read_html(finviz_page[start:end], header=0)[0]
                 else:
                     df = df.append(pd.read_html(finviz_page[start:end], header=0)[0])
-                break
-            break
-
         df = df.reset_index()
 
         # get all the quotes relating to each stock, for other related data
         urls = []
         for symbol in df['Ticker']:
             urls.append('https://finviz.com/quote.ashx?t=' + symbol)
-
         p = pool.Pool.from_urls(urls, num_processes=20)
         p.join_all()
 
@@ -127,19 +117,19 @@ class finviz_alerts(object):
             df = pd.read_html(response.content[start:end])[0]
             df = pd.DataFrame(df.values.reshape(-1, 2), columns=['key', index_num])
             df = df.set_index('key').T
-            df = df[self.total_features_set]
+            df = df[self.total_features_set+['Price']]
             df = df.replace('-', df.replace(['-'], [None]))
             df['Ticker'] = symbol
+            df['Start_Date'] = datetime.now().strftime("%b %d, %Y")
             self.company_data.append(df)
             index_num+=1
         self.company_data = pd.concat(self.company_data)
 
         # format data
-        for col in self.company_data.columns[:-1]:
+        for col in self.company_data[self.total_features_set]:
             if self.company_data[col].str.contains('%').any():
                 self.company_data[col] = self.p2f(self.company_data[col])
             self.company_data[col] = self.company_data[col].apply(pd.to_numeric)
-        print(self.company_data)
 
     def get_backtest_dataset(self):
         df = pd.read_csv('nosql_data.csv')
@@ -147,34 +137,6 @@ class finviz_alerts(object):
         col_str = str(df.columns).replace('\/','/').split('[')[1].split(']')[0]
         df.columns = eval('['+col_str+']')
         self.total_dataset = df[self.total_features_set]
-
-    def get_company_data(self, response):
-        # formats all the other related data
-
-        #'Symbol', 'Date', 'Signal', 'Wk', 'Mth', 'Vol_Chg',
-        symbol = response.request_kwargs['url'].split('=')[1]
-        date = datetime.now().strftime("%b %d, %Y")
-        signal = float(finviz_data['Change'][:-1])/100
-        price = float(finviz_data['Price'])
-        avg_vol = self.convert_to_float(finviz_data['Avg Volume'])
-        vol = float(finviz_data['Volume'])
-        vol_chg = (vol-avg_vol)/avg_vol
-        try:
-            chg_wk = float(finviz_data['Perf Week'][:-1])/100
-        except:
-            chg_wk = None
-        try:
-            chg_mth = float(finviz_data['Perf Month'][:-1])/100
-        except:
-            chg_mth = None
-        try:
-            chg_qtr = float(finviz_data['Perf Quarter'][:-1])/100
-        except:
-            chg_qtr = None
-
-        company_data = [symbol, date, signal, avg_vol, vol, vol_chg, chg_wk, chg_mth, chg_qtr, price]
-
-        return company_data
 
     def convert_to_float(self, value):
         if 'K' in value:
@@ -184,7 +146,6 @@ class finviz_alerts(object):
         elif 'B' in value:
             value = float(value[:-1])*1000000000
         return float(value)
-
 
 class closer(object):
     def __init__(self):
@@ -215,4 +176,6 @@ class closer(object):
                 cur = conn.cursor()
                 cur.execute('update alerts set Close_Price = %f, Percent_Change = %f where Symbol="%s" and Close_Date="%s"' % (close_price, percent_change, symbol, close_date))
             conn.commit()
-finviz_alerts()
+
+#finviz_alerts()
+#closer()
